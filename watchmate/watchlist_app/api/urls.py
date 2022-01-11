3695172e8ba18d2c5ffe72d392f3263ec4eae5c6
himangshuishere <dehimangshu2020@outlook.com>
2022-01-11 12:11:51 +0530

    Added two Models and updated them in views and serializers

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -3,6 +3,7 @@ from watchlist_app.api.views import *
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
-    path('<int:pk>', WatchDetailAV.as_view(), name='movie-detail'),
+    path('list/<int:pk>', WatchDetailAV.as_view(), name='movie-detail'),
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
+    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream')
 ]
