67ccd6bc9196db43326c9c1e446aca1dbfd7176e
himangshuishere <dehimangshu2020@outlook.com>
2022-01-30 13:00:37 +0530

    Deleted previous branch projects

--- Notes/README.md
+++
@@ -1,4 +0,0 @@
-# Notes App Using Django
-This is my own project which I am doing as a practice after completing django-rest tutorials on Udemy.
-
-## PRs are always welcomed...
--- watchmate/watchlist_app/api/throttling.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.throttling import UserRateThrottle
-
-class ReviewCreateThrottle(UserRateThrottle):
-    scope = 'review-create'
-
-class ReviewListThrottle(UserRateThrottle):
-    scope = 'review-list'
